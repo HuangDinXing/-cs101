30ac460e5f6de73856cbe84f7c6dcef02086168a
b1129042 <68554413+HuangDinXing@users.noreply.github.com>
2022-12-28 01:42:25 +0800

    Update main1.c

--- HW6/main1.c
+++ HW6/main1.c
@@ -2,17 +2,17 @@ int main()
 {
     int n=10;
     
-    if(n%15==0){
+    if(n%15==0)
         printf("Love IU");
-    }
-    else if(n%5==0){
+    
+    else if(n%5==0)
         printf("IU");
-    }
-    else if(n%3==0){
+    
+    else if(n%3==0)
         printf("Love");
-    }
-    else{
+    
+    else
         printf("n=%d",n);
-    }
+    
     return 0;
 }
