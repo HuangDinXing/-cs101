36e37b31fb28ffe3dabeb8cdfed05015c56236cb
b1129042 <68554413+HuangDinXing@users.noreply.github.com>
2023-01-09 17:01:43 +0800

    Update main1.c

--- HW8/main1.c
+++ HW8/main1.c
@@ -2,15 +2,13 @@
 
 int main(){
     int number=10;
-    int a;
-    for(int i=1;i<number;i++){
-        for(int n=number;n>i;n--){
+    for (int i=0;i<number;i++){
+        for(int a=10;a>i;a--){
             printf(" ");
         }
-        for(a=1;a<=i;a++){
-            printf("* ");
+        for(int b=0;b<=i*2;b++){
+            printf("*");
         }
-        
-         printf("\n");
+        printf("\n");
     }    
 }
