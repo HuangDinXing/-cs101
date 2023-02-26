210883a5774b14a22cec89f10d700929840868a9
b1129042 <68554413+HuangDinXing@users.noreply.github.com>
2023-02-27 01:24:29 +0800

    Update main5.c

--- HW1/main5.c
+++ HW1/main5.c
@@ -3,24 +3,24 @@
 int main() {
     char a[] = {-9, 8, 7, 6, 5, -4, 3, -2, 1};
     int i = 4;
-    int n = sizeof(a) / sizeof(char); // 計算陣列大小
+    int n = sizeof(a) / sizeof(char); 
 
-    for (int k = 0; k < i; k++) { // 逐步找出前 i 大的數
+    for (int k = 0; k < i; k++) { 
         char max = a[0];
         int max_index = 0;
 
-        for (int j = 1; j < n; j++) { // 找出目前未找到的最大值
+        for (int j = 1; j < n; j++) { 
             if (a[j] > max) {
                 max = a[j];
                 max_index = j;
             }
         }
 
-        a[max_index] = a[n-1]; // 將已找到的最大值移至陣列末端
-        n--; // 更新未找到最大值的陣列大小
+        a[max_index] = a[n-1];
+        n--; 
     }
 
-    printf("%d\n", (int)a[n]); // 最後剩下的數即為第 i 大的數
+    printf("%d\n", (int)a[n]); 
 
     return 0;
 }
